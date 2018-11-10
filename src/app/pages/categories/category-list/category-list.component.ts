21c502aef7f460869202f91a2d72c13d329f3b6d
jaironsousa1 <jaironsousa@gmail.com>
2018-11-10 15:09:16 -0300

    16 - Concluindo a implementação do 'CategoryList'

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(
+    private categoriaService: CategoryService
+  ) { }
 
   ngOnInit() {
+    this.categoriaService.getAll()
+      .subscribe(
+        categories => this.categories = categories,
+        error => alert('Erro ao carregar alista')
+      );
+  }
+
+  delete(category: Category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if (mustDelete) {
+      this.categoriaService.delete(category.id)
+        .subscribe(
+          () => this.categories = this.categories.filter(element => element !== category),
+          () => alert('Erro ao excluir')
+        );
+    }
   }
 
 }
